3b411c073b56a93d591807e19a11ea4ada8788e9
4c <4c@INF07>
2022-04-27 11:45:48 +0200

    versione 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * 
+ * @author Francesco
+ * 
+ * @version1
+ * 
+ * 
+ */
+public class Main {
+
+	   public static void main(String[] args) {
+		   
+		 System.out.println("Prova1");
+	   }
+}
